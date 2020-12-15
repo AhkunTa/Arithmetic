9d705fa2f4fb4270fca6ba772f98134a021b5c22
AhkunTa <gqk1213@foxmail.com>
2020-12-15 17:17:37 +0800

    add 12 modify 13

--- LeetCode/13_javascript.js
+++ LeetCode/13_javascript.js
@@ -13,57 +13,74 @@
 // 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
 
 // I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
-// X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+// X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
 // C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 // 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
-
-
 
 /**
  * @param {string} s
  * @return {number}
  */
+// 法一
+var romanToInt = function (s) {
+  var result = 0;
+  if (s.includes("IV") || s.includes("IX")) {
+    result = result - 2;
+  }
+  if (s.includes("XL") || s.includes("XC")) {
+    result = result - 20;
+  }
+  if (s.includes("CD") || s.includes("CM")) {
+    result = result - 200;
+  }
 
- 
-var romanToInt = function(s) {
-    var result = 0;
-    if(s.includes('IV') || s.includes('IX')){
-        result =result -2; 
+  for (let c of s) {
+    switch (c) {
+      case "I":
+        result += 1;
+        break;
+      case "V":
+        result += 5;
+        break;
+      case "X":
+        result += 10;
+        break;
+      case "L":
+        result += 50;
+        break;
+      case "C":
+        result += 100;
+        break;
+      case "D":
+        result += 500;
+        break;
+      case "M":
+        result += 1000;
+        break;
     }
-    if(s.includes('XL') || s.includes('XC')){
-        result =result -20; 
+  }
+  return result;
+};
+
+// 法二
+// 依次判断 左边的值小于右边减去 大于右边加上
+var romanToInt = function (s) {
+  let map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < map[s[i + 1]] && i + 1 < s.length) {
+      result -= map[s[i]];
+    } else {
+      result += map[s[i]];
     }
-    if(s.includes('CD') || s.includes('CM')){
-        result =result -200; 
-    }
-    
-    for(let c of s ){
-        switch(c){
-            case "I":
-                result +=1;
-                break;
-            case "V":
-                result +=5;
-                break;
-            case "X":
-                result +=10;
-                break;
-            case "L":
-                result +=50;
-                break;
-            case "C":
-                result +=100;
-                break;
-            case "D":
-                result +=500;
-                break;
-            case "M":
-                result +=1000;
-                break;
-                
-    }
-       
-}
-     return result;
-    
+  }
+  return result;
 };
