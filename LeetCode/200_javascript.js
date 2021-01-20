d29e84e6f31cfc7f64af1439c0dbfa84296e00a3
AhkunTa <gqk1213@foxmail.com>
2021-01-20 19:25:42 +0800

    add 200 bfs solution

--- LeetCode/200_javascript.js
+++ LeetCode/200_javascript.js
@@ -36,6 +36,8 @@
  * @param {character[][]} grid
  * @return {number}
  */
+
+// DFS
 var numIslands = function (grid) {
   let res = 0;
 
@@ -62,6 +64,49 @@ var numIslands = function (grid) {
       // 一旦存在一个 1 则所有其相邻岛屿都为 2 包括自己
       if (grid[i][j] == "1") {
         dfs(i, j);
+        res++;
+      }
+    }
+  }
+  return res;
+};
+
+// BFS
+var numIslands = function (grid) {
+  let res = 0;
+  let tempArray = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      // 遍历数组当前为1的时候 dfs循环将所有此岛上下左右的岛感染 将岛值 置为2
+      // 一旦存在一个 1 则所有其相邻岛屿都为 2 包括自己
+      if (grid[i][j] == "1") {
+        grid[i][j] = "2";
+        tempArray.push([i, j]);
+        while (tempArray.length) {
+          let temp = tempArray.shift();
+          let direction = [
+            [-1, 0],
+            [1, 0],
+            [0, -1],
+            [0, 1],
+          ];
+          for (let [a, b] of direction) {
+            let i = temp[0] + a;
+            let j = temp[1] + b;
+            if (
+              i < 0 ||
+              i > grid.length - 1 ||
+              j < 0 ||
+              j > grid[0].length - 1 ||
+              // 判断 为 2 或者 0 跳出循环避免重复判断
+              grid[i][j] != "1"
+            ) {
+              continue;
+            }
+            grid[i][j] = "2";
+            tempArray.push([i, j]);
+          }
+        }
         res++;
       }
     }
