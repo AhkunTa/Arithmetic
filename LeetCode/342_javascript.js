fef5875db9485a5d873a772c7d8d9cc7c977f590
AhkunTa <gqk1213@foxmail.com>
2021-06-01 00:10:44 +0800

    modify

--- LeetCode/342_javascript.js
+++ LeetCode/342_javascript.js
@@ -44,3 +44,15 @@ var isPowerOfFour = function (n) {
     return false;
   }
 };
+// 如果 n 是 44 的幂，那么它可以表示成 4^x的形式，我们可以发现它除以 3 的余数一定为 1
+// n & (n - 1) 表示一定为2的幂
+var isPowerOfFour = function (n) {
+  return n > 0 && (n & (n - 1)) === 0 && n % 3 === 1;
+};
+
+var isPowerOfFour = function (n) {
+  return n > 0 && (n & (n - 1)) === 0 && (n & 0xaaaaaaaa) === 0;
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/power-of-four/solution/4de-mi-by-leetcode-solution-b3ya/
