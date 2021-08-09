fffbfc3fcb9b9b9a409c3a455ea8043ca0519f9a
AhkunTa <gqk1213@foxmail.com>
2021-08-09 19:28:42 +0800

    modify 313

--- LeetCode/313_javascript.js
+++ LeetCode/313_javascript.js
@@ -21,6 +21,16 @@
  * @param {number[]} primes
  * @return {number}
  */
+
+// 例如 n = 10， primes = [2,7,13,19]。 打印出丑数列表：1,2,4,7,8,13,14,16,19,26,28,32
+// 所有 除第一位为1 之外之后的所有丑数 都是由第一位 乘2 或7 或13 或19 的来的
+// 每次寻找丑数的时候 判断当前
+// 第一个丑数为1
+// 第二个丑数为 dp[0] * 2    dp[0] * 7  dp[0] * 13   dp[0] * 19  中最小的一个数 为2
+// 第三个丑数   dp[1] * 2    dp[0] * 7  dp[0] * 13   dp[0] * 19  为4
+// 第四个丑数   dp[2] * 2    dp[0] * 7  dp[0] * 13   dp[0] * 19  为7
+// 避免重复计算 需记录每一次的指针
+
 // 将三指针转化为n指针
 var nthSuperUglyNumber = function (n, primes) {
   let dp = new Array(n).fill(0);
@@ -28,13 +38,14 @@ var nthSuperUglyNumber = function (n, primes) {
   dp[0] = 1;
   for (let i = 1; i < n; i++) {
     let min = Number.MAX_SAFE_INTEGER;
-
+    // 初始 arr = [1,1,1,1]
     for (let j = 0; j < primes.length; j++) {
       min = Math.min(min, dp[arr[j] - 1] * primes[j]);
     }
-
+    // 获取最小值
     dp[i] = min;
     for (let k = 0; k < primes.length; k++) {
+      // 此处记录指针
       if (min == dp[arr[k] - 1] * primes[k]) {
         arr[k]++;
       }
