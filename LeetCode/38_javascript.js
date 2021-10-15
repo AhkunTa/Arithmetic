f9b12486d25b24455dba366eb262b6b84d350001
AhkunTa <gqk1213@foxmail.com>
2021-10-15 21:55:03 +0800

    modify 38

--- LeetCode/38_javascript.js
+++ LeetCode/38_javascript.js
@@ -22,7 +22,7 @@
 // 输入: 4
 // 输出: "1211"
 
-// 题目理解有点问题 其实是这样的 
+// 题目理解有点问题 其实是这样的
 // 第一位 ‘1’ 读一个一  所以第二位 ‘11’
 // 第二位 ‘11’ 读二个一  所以第三位 ‘21’
 // 以此类推
@@ -33,31 +33,59 @@
 // 5.     111221
 // 6.     312211
 
+var countAndSay = function (n) {
+  var arr = [1];
+  function toArray(arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      var num = 1; //重复次数
+      var index = arr[i]; //当前数字
 
-var countAndSay = function(n) {
-	var arr = [1];
-    function toArray(arr){
-        var newArr = [];
-        for(var i=0; i<arr.length; i++){
-
-            var num = 1; //重复次数
-            var index = arr[i] //当前数字
-
-            //循环判断后一位是否等于前一位数字
-            //若相等则 重复次数+1 再次判断再后一位是否与前一位相等
-            while(arr[i+1] == index ){
-                num ++;
-                i++;
-            }
-           	// 将数字存入数组
-            newArr.push(num,index);
-        }
-        return newArr;
+      //循环判断后一位是否等于前一位数字
+      //若相等则 重复次数+1 再次判断再后一位是否与前一位相等
+      while (arr[i + 1] == index) {
+        num++;
+        i++;
+      }
+      // 将数字存入数组
+      newArr.push(num, index);
     }
-    for(var j=1; j<n; j++){
-        arr = toArray(arr);
-    }
-	return arr.join('');
+    return newArr;
+  }
+  for (var j = 1; j < n; j++) {
+    arr = toArray(arr);
+  }
+  return arr.join("");
 };
 
-//2 
+// 2 动态规划
+// 也不算动态规划 就是根据上一个 求下一个递推
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function (n) {
+  let dp = [];
+  dp[0] = "1";
+  for (let i = 1; i < n; i++) {
+    let str = dp[i - 1];
+    let dpStr = "";
+    let len = 1;
+    for (let j = 0; j < str.length - 1; j++) {
+      if (str[j] !== str[j + 1]) {
+        // 当前后两个值不相等的时候 截断
+        // 并且len 重置
+        dpStr += len.toString() + str[j];
+        len = 1;
+      } else {
+        len++;
+      }
+    }
+    // 注意这里需要判断最后一位 加上去
+    // 即 当 21 的时候
+    // 截断 了 2  但是 1 还需再计算一下 才有 1211
+    dpStr += len.toString() + str[str.length - 1];
+    dp[i] = dpStr;
+  }
+  return dp[n - 1];
+};
